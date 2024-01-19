6b3bfcefa5e8e834ecb092c0c426f96614db411b
Ganuuh <145634432+Ganuuh@users.noreply.github.com>
2024-01-19 11:02:30 +0800

    init

--- frontend/src/common/index.js
+++ frontend/src/common/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8008",
+  baseURL: "https://income-expense-sjgp.onrender.com/",
   headers: {
     "Content-Type": "application/json",
   },
